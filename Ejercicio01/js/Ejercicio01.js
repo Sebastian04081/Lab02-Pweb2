bb550ba306ef8453baf8da62e5c92f1bbfcfd515
Sebastian Diaz Ticona <sdiazt@unsa.edu.pe>
2023-05-11 19:12:24 -0500

    Se añadio la funcion getDay

---
+++ Ejercicio01/js/Ejercicio01.js
@@ -0,0 +1,31 @@
+var numberDay;
+var date = new Date();
+
+function getDay(numberDay = date.getDay()) {
+	let day;
+	switch (numberDay) {
+		case 0:
+			day = "Domingo";
+			break;
+  		case 1:
+			day = "Lunes";
+			break;
+  		case 2:
+			day = "Martes";
+			break;
+  		case 3:
+			day = "Miercoles";
+			break;
+  		case 4:
+			day = "Jueves";
+			break;
+  		case 5:
+			day = "Viernes";
+			break;
+  		case 6:
+    			day = "Sabado";
+			break;
+	}
+
+    console.log(day);
+}
