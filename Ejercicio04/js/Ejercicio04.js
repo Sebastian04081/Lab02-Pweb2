528f6eaa07ef6bfa1d4e3b753cff974550529e74
Sebastian Diaz Ticona <sdiazt@unsa.edu.pe>
2023-05-15 11:44:34 -0500

    Se mejoro la funcion getCode()

--- Ejercicio04/js/Ejercicio04.js
+++ Ejercicio04/js/Ejercicio04.js
@@ -1,11 +1,14 @@
-
+// Función para obtener el código de la sesión de Google Meet a partir de la URL ingresada
 function getCode() {
     // Obtiene el valor del input con el URL de la sesión
 	const url = document.querySelector("#url").value;
     console.log(url);
 
+    // Dividir la cadena del URL en la primera aparición del separador de la cadena de consulta (?)
+    const codeWithQueryParams = url.split("?")[0];
+
     // Dividir la cadena de la URL en partes usando el carácter "/"
-    const parts = url.split("/");
+    const parts = codeWithQueryParams.split("/");
 
     // Obtener la última parte de la URL, que corresponde al código de la sesión
     const code = parts[parts.length - 1];
