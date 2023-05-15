d29c9ceb9fe709740fd16f741d48489f2aa03c42
Sebastian Diaz Ticona <sdiazt@unsa.edu.pe>
2023-05-15 12:10:48 -0500

    Se añadio una funcion para copiar el codigo copyCode()

--- Ejercicio04/js/Ejercicio04.js
+++ Ejercicio04/js/Ejercicio04.js
@@ -5,9 +5,11 @@ function getCode() {
     console.log(url);
 
     // Dividir la cadena del URL en la primera aparición del separador de la cadena de consulta (?)
+    // https://meet.google.com/pxg-cvfx-don?pli=1&authuser=0
     const codeWithQueryParams = url.split("?")[0];
 
     // Dividir la cadena de la URL en partes usando el carácter "/"
+    // https://meet.google.com/pxg-cvfx-don
     const parts = codeWithQueryParams.split("/");
 
     // Obtener la última parte de la URL, que corresponde al código de la sesión
@@ -18,4 +20,34 @@ function getCode() {
 
     // Actualizar el contenido del elemento HTML con el código sin separadores
     document.querySelector("#code").textContent = codeWithoutSeparators;
+}
+
+// Función para copiar el código de la sesión al portapapeles del usuario
+function copyCode() {
+    // Obtén la referencia al elemento que contiene el código de la sesión
+    const codeOutput = document.getElementById("code");
+
+    // Obtiene el texto del código de la sesión
+    const code = codeOutput.textContent;
+
+    // Crea un elemento de área de texto temporal
+    const tempTextArea = document.createElement("textarea");
+
+    // Asigna el valor del código de la sesión al área de texto temporal
+    tempTextArea.value = code;
+
+    // Agrega el área de texto temporal al cuerpo del documento
+    document.body.appendChild(tempTextArea);
+
+    // Selecciona el contenido del área de texto temporal
+    tempTextArea.select();
+
+    // Copia el contenido al portapapeles del usuario
+    document.execCommand("copy");
+
+    // Remueve el área de texto temporal del documento
+    document.body.removeChild(tempTextArea);
+
+    // Muestra una alerta para indicar que el código de la sesión ha sido copiado correctamente
+    alert("El código de la sesión se ha copiado al portapapeles.");
 }
